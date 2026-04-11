203808e19c4195ee521b536306a33c70b7112bf5
DATA-WELLERSON <data.wellerson@gmail.com>
2026-04-11 11:45:56 -0300

    adicionando o kagglehub no requirements.txt

--- src/Extract/extract.py
+++ src/Extract/extract.py
@@ -1,8 +1,25 @@
-from pathlib import Path
-
 import pandas as pd
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
 
 
-def extract(csv_path: Path) -> pd.DataFrame:
-    """Extrai dados de um CSV para um DataFrame."""
-    return pd.read_csv(csv_path)
+class Extractor:
+    """Classe responsável por extrair dados de diferentes fontes para um DataFrame."""
+
+    def __init__(self, file_path: str, elemento: str):
+        self.file_path = file_path
+        self.elemento = elemento
+
+    def read_from_kaggle(self) -> pd.DataFrame:
+        """Carrega o dataset do Kaggle e retorna um DataFrame."""
+        file_path = self.file_path
+
+        df = kagglehub.load_dataset(
+            KaggleDatasetAdapter.PANDAS,
+            self.elemento,
+            file_path,
+        )
+
+        return df
+
+    
